32a8a4929243c9b4778eda541aef17bee7bdc35d
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-11-11 23:44:32 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, Text } from 'ink';
+
+function App() {
+    return (
+        <>
+            <Text color="green">Hello, World!</Text>
+        </>
+    );
+}
+
+render(<App />);
