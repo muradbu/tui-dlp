590e32109663e43d21425026e72cbba43a8b16b8
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2022-11-12 04:01:08 +0100

    feat: created text input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Text } from 'ink';
 import { UncontrolledTextInput } from 'ink-text-input';
+import clipboard from "clipboardy";
 
-export default function UrlBar() {
+type Props = {
+    clipboardAutoFill?: boolean;
+}
 
+function isUrl() {
+    const value = clipboard.readSync();
+    if (value.match(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/)) {
+        return value
+    }
 
+    return ''
+}
+
+export default function UrlBar({ clipboardAutoFill = false }: Props) {
     return (
         <Box borderStyle="round" paddingX={1}>
             <Box marginRight={1}>
                 <Text color="green">URL:</Text>
             </Box>
-            <UncontrolledTextInput />
+            <UncontrolledTextInput initialValue={clipboardAutoFill ? isUrl() : ''} />
         </Box>
     );
 }
